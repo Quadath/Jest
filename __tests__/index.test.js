2bb2fce0df3281830a81f3cf0fc9da1a71b730f5
Quadatz <quadath@gmail.com>
2022-08-14 09:03:46 +0300

    second test + import statemenent

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,6 @@
-const sum = require('../src/index');
+import reverse from '../src/index.js';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('reverse', () => {
+  expect(reverse('hello')).toEqual('olleh');
+  expect(reverse('')).toEqual('');
 });
